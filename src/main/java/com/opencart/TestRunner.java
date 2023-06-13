48efd0306b3fd1628301e229100b68ba7ba3b05f
Petru <petrupascari1994@gmail.com>
2023-06-13 17:44:42 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,19 +1,42 @@
 package com.opencart;
 
+import com.opencart.Managers.DriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class TestRunner {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://999.md/ru/");
+        Thread.sleep(5000);
+
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        Thread.sleep(5000);
+
+        driver.get("https://diez.md/");
+
+
+        driver.quit();
+
+        System.out.println("The Execution was finished");
+
+
+
+
+
+
     }
 }
