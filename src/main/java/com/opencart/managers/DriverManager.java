414cad9723aefe47022840ce317a238895ea1934
Petru <petrupascari1994@gmail.com>
2023-06-27 20:40:54 +0300

    Added 2 scenarios using cucumber and Gerkin

--- src/main/java/com/opencart/managers/DriverManager.java
+++ src/main/java/com/opencart/managers/DriverManager.java
@@ -3,6 +3,7 @@ package com.opencart.managers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,8 +15,9 @@ public class DriverManager {
     DriverManager() {
         switch (webDriverType.toUpperCase()) {
             case "CHROME":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--incognito");
+                driver = new ChromeDriver(options);
                 System.out.println("The Chrome Driver was initiated!");
                 break;
 
