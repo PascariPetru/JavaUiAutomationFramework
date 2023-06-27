ffffcb078ff5d28ea02392562152b24e23638c00
Petru <petrupascari1994@gmail.com>
2023-06-27 19:54:37 +0300

    Added 2 scenarios using cucumber and Gerkin

--- src/main/java/com/opencart/stepdefinitions/HomePageSteps.java
+++ src/main/java/com/opencart/stepdefinitions/HomePageSteps.java
@@ -1,5 +1,23 @@
 package com.opencart.stepdefinitions;
 
+import com.opencart.managers.DriverManager;
+import com.opencart.pageobjects.HomePage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+
 public class HomePageSteps {
 
+    WebDriver driver = DriverManager.getInstance().getDriver();
+    HomePage homePage = new HomePage(driver);
+
+    @Given("Home Page is accessed")
+    public void homePageIsAccessed() {
+        DriverManager.getInstance().getDriver().get("https://andreisecuqa.host/");
+    }
+
+    @And("RegisterPage is accessed from HomePage menu")
+    public void registerpageIsAccessedFromHomePageMenu() {
+
+    }
 }
